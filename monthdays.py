3b9c9b45a6ce6a3106ad60f65c37f0ded9ac7d3c
muhammadyousha <167106351+muhammadyousha@users.noreply.github.com>
2024-04-16 21:07:11 +0500

    months in a year

---
+++ monthdays.py
@@ -0,0 +1,28 @@
+month=1
+if month ==1:
+    print('january 31 days')
+if month==2 :
+    print('february 28 days')
+if month==3 :
+    print('march 31 days')
+if month==4 :
+    print('april 30 days')
+if month==5 :
+    print('may 31 days')
+if month==6 :
+    print('june 30 days')
+if month==7 :
+    print('july 31 days')
+if month==8 :
+    print('august 31 days')
+if month==9 :
+    print('september 30 days')
+if month==10 :
+    print('october 31 days')
+if month==11 :
+    print('november 30 days')
+if month==12 :
+    print('december 31 days')
+
+
+    
