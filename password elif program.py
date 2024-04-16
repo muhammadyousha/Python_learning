065becea82c13d86010730623165dd6df93820f7
muhammadyousha <167106351+muhammadyousha@users.noreply.github.com>
2024-04-16 21:15:13 +0500

    password through elif program

---
+++ password elif program.py
@@ -0,0 +1,9 @@
+password=1234
+
+if password==1234 :
+    print('ACCESS GRANTED TO YOUSHA SYSTEM')
+elif password==1234 :
+    print('ACCESS GRANTED TO abuzar SYSTEM')
+elif password== 1234 :
+    print('ACCESS GRANTED TO muaz SYSTEM')
+
