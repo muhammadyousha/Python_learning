53987e856c2784e6d35f49cb2a681bc399b12d43
muhammadyousha <167106351+muhammadyousha@users.noreply.github.com>
2024-04-16 23:11:12 +0500

    divisible loops

---
+++ oddevenloops.py
@@ -0,0 +1,8 @@
+com='non divisible'
+for i in range (0,50):
+    if com=='divisible':
+        if i%2==0:
+            print(i)
+    if com=='non divisible':
+        if i%2!=0:
+            print(i)
