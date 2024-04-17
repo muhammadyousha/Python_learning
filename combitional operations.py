e13831d73dfb3a1e488f3912c7bb4568ec8d2990
muhammadyousha <167106351+muhammadyousha@users.noreply.github.com>
2024-04-17 20:13:59 +0500

    combinational and operation

---
+++ combitional operations.py
@@ -0,0 +1,21 @@
+x='true'
+y='false'
+print(x=='true' and y=='false')
+
+x=True
+f=False
+print(x!=True and y==False)
+
+x=4
+y=2
+print(x+y==6 and x*y==8)
+
+x='7'
+y='book'
+print(x+y=='7book' and x+y!='7book')
+
+
+
+
+
+
